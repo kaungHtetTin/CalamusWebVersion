5391f8260c71f71358f862b352445fee06f027dd
kaung htet <kaunghtettin17204@gmail.com>
2025-11-19 00:27:15 +0630

    flash_card_ok

--- development/add_word.php
+++ development/add_word.php
@@ -5,6 +5,9 @@ $json = file_get_contents('korean_words.json');
 $data = json_decode($json, true);
 
 $db = new Database();
+$language_id = 1; // English language ID - update this based on your languages table
+$deck_id = 1; // Deck ID - update this based on your decks table
+
 
 foreach($data as $item){
     $korean_word = $item['Korean word'];
@@ -13,7 +16,7 @@ foreach($data as $item){
     $korean_example_2 = $item['Korean Example 2'];
     $example_sentences = json_encode([$korean_example_1, $korean_example_2]);
 
-    $query = "INSERT INTO cards (word, burmese_translation, example_sentences) VALUES ('$korean_word', '$burmese_translation', '$example_sentences')";
+    $query = "INSERT INTO cards (word, burmese_translation, example_sentences, language_id, deck_id) VALUES ('$korean_word', '$burmese_translation', '$example_sentences', $language_id, $deck_id)";
     $result = $db->save($query);
     if($result){
         echo "Word added successfully: $korean_word\n";

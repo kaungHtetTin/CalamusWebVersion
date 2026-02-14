955e278466b3485af4a51ca21dad2fc7317b74f6
kaung htet <kaunghtettin17204@gmail.com>
2026-02-14 22:00:20 +0630

    this_is_react_version

---
+++ config.js
@@ -0,0 +1,6 @@
+/**
+ * Deployment config: https://domain.com/calamus (app) + https://domain.com/calamus/api (API)
+ * Edit this file after build if needed; no rebuild required.
+ */
+window.__BASE_PATH__ = '/calamus';
+window.__API_BASE_URL__ = window.location.origin + '/calamus/api';

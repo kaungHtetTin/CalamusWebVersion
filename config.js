a1f2a0ce4aedf983ea269ed91d49102780df8971
kaung htet <kaunghtettin17204@gmail.com>
2026-04-23 01:59:53 +0630

    dir_adjust

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /**
- * Deployment config: https://domain.com/calamus (app) + https://domain.com/calamus/api (API)
+ * Deployment config: https://domain.com/calamus (app) + https://domain.com/calamus-v3/public/api (API)
  * Edit this file after build if needed; no rebuild required.
  */
 window.__BASE_PATH__ = '/calamus';
-window.__API_BASE_URL__ = 'http://localhost/calamus-v3/public/api';
+window.__API_BASE_URL__ = 'https://www.calamuseducation.com/calamus-v3/public/api';
